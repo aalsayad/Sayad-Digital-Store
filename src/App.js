ebc29f237adfc2198c3b5312fa05a3023034d969
aalsayad <74960405+aalsayad@users.noreply.github.com>
2022-04-16 00:57:52 +0300

    Finalized Moving Categories to their own component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
-import "./App.css";
-import Category from "./Components/Category/Category";
-import categoriesData from "./Components/Category/categoriesData";
-import "./Components/Category/categories.styles.scss";
+import React from "react";
+import Categories from "./Components/Category/Categories";
 
 const App = () => {
-  const mappedCategories = () => {
-    const mapped = categoriesData.map(({ id, title, subtitle, img }) => {
-      return <Category key={id} title={title} subtitle={subtitle} img={img} id={id} />;
-    });
-    return mapped;
-  };
-
   return (
     <>
-      <div className="categories-container">{mappedCategories()}</div>
+      <Categories />
     </>
   );
 };
---
+++ src/Components/Category/Categories.jsx
@@ -0,0 +1,20 @@
+import Category from "./Category";
+import categoriesData from "./categoriesData";
+import "./categories.styles.scss";
+
+const Categories = () => {
+  const mappedCategories = () => {
+    const mapped = categoriesData.map(({ id, title, subtitle, img }) => {
+      return <Category key={id} title={title} subtitle={subtitle} img={img} id={id} />;
+    });
+    return mapped;
+  };
+
+  return (
+    <>
+      <div className="categories-container">{mappedCategories()}</div>
+    </>
+  );
+};
+
+export default Categories;
