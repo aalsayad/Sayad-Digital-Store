db34acfccbd99824b694bd87f1e5ffad0395f3bf
aalsayad <74960405+aalsayad@users.noreply.github.com>
2022-04-16 17:35:24 +0300

    Trial for mobile video rendering

--- src/Components/HomeVideo/HomeVideo.jsx
+++ src/Components/HomeVideo/HomeVideo.jsx
@@ -1,9 +1,36 @@
 import "./homevideo.styles.scss";
+import Video from "./home-video.webm";
 
 const HomeVideo = () => {
+  // const EmbedVideo = function (props) {
+  //   return (
+  //     <div
+  //       dangerouslySetInnerHTML={{
+  //         __html: `
+  //       <video
+  //         loop
+  //         muted
+  //         autoplay
+  //         playsinline
+  //         src="${props.src}"
+  //         class="${props.className}"
+  //       />,
+  //     `,
+  //       }}
+  //     ></div>
+  //   );
+  // };
   return (
     <div className="video-container">
-      <video className="video" autoPlay loop muted>
+      {/* <div
+        dangerouslySetInnerHTML={{
+          __html: `<video className="video" autoplay loop muted playsinline>
+      <source src=${Video} type="video/webm" />
+      Your browser does not support the video tag.
+</video> `,
+        }}
+      /> */}
+      <video className="video" autoPlay loop muted playsInline>
         <source src="./assets/videos/home-video.webm" type="video/webm" />
       </video>
       <div className="video-text">
