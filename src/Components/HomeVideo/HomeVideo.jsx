a41ca8a3d810386bf6b8500de7f04659bb46a237
aalsayad <74960405+aalsayad@users.noreply.github.com>
2022-04-16 19:41:32 +0300

    Switched WEBM video to MP4, now works on iphones

--- src/Components/HomeVideo/HomeVideo.jsx
+++ src/Components/HomeVideo/HomeVideo.jsx
@@ -1,46 +1,21 @@
 import "./homevideo.styles.scss";
-import Video from "./home-video.webm";
 
 const HomeVideo = () => {
-  // const EmbedVideo = function (props) {
-  //   return (
-  //     <div
-  //       dangerouslySetInnerHTML={{
-  //         __html: `
-  //       <video
-  //         loop
-  //         muted
-  //         autoplay
-  //         playsinline
-  //         src="${props.src}"
-  //         class="${props.className}"
-  //       />,
-  //     `,
-  //       }}
-  //     ></div>
-  //   );
-  // };
   return (
-    <div className="video-container">
-      {/* <div
-        dangerouslySetInnerHTML={{
-          __html: `<video className="video" autoplay loop muted playsinline>
-      <source src=${Video} type="video/webm" />
-      Your browser does not support the video tag.
-</video> `,
-        }}
-      /> */}
-      <video className="video" autoPlay loop muted playsInline>
-        <source src="./assets/videos/home-video.webm" type="video/webm" />
-      </video>
-      <div className="video-text">
-        <h2>
-          Be exclusive, <br />
-          <span className="strong">Be Bold,</span>
-          <br /> Be yourself.
-        </h2>
+    <>
+      <div className="video-container">
+        <video className="video" autoPlay loop muted playsInline>
+          <source src="./assets/videos/sayad-store.mp4" type="video/mp4" />
+        </video>
+        <div className="video-text">
+          <h2>
+            Be exclusive, <br />
+            <span className="strong">Be Bold,</span>
+            <br /> Be yourself.
+          </h2>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
