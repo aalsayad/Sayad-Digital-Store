cf70a31dfc20902286fb4f0bf4b8b1fd25e380ca
aalsayad <74960405+aalsayad@users.noreply.github.com>
2022-04-18 12:40:12 +0300

    fix error of vercel app

---
+++ src/Utilities/Firebase/Firebase.ulitities.js
@@ -0,0 +1,24 @@
+// import { initializeApp } from "firebase/app";
+// import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
+// //!Link firebase to this account + make it point to the exact firebase project you want
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDOkmbUCdez05-jZlb9IAI4oxw8r-aRnZo",
+//   authDomain: "sayad-store-db.firebaseapp.com",
+//   projectId: "sayad-store-db",
+//   storageBucket: "sayad-store-db.appspot.com",
+//   messagingSenderId: "982583278534",
+//   appId: "1:982583278534:web:c14969156e476316c610c7",
+// };
+
+// const firebaseApp = initializeApp(firebaseConfig);
+
+// //!Authentication
+// //Starting with google
+// const provider = new GoogleAuthProvider();
+// provider.setCustomParameters({
+//   prompt: "select_account",
+// });
+
+// export const auth = getAuth();
+// export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
