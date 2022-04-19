11f4df61f61b4a1716d48dabaf1a7100e6dbaa5e
aalsayad <74960405+aalsayad@users.noreply.github.com>
2022-04-19 22:51:46 +0300

    experimenting with animations

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -12,31 +12,33 @@ const SignInForm = () => {
     createUserDocumentFromAuth(user);
   };
   return (
-    <>
-      <div className="form-container form--login">
-        <div className="form-title">
-          <h2>Log in</h2>
+    <main>
+      <div>
+        <div className="form-container form--login">
+          <div className="form-title">
+            <h2>Log in</h2>
+          </div>
+
+          <form className="form-fields-container-login">
+            <div>
+              <label>Email</label>
+              <input type="email" required name="email" />
+              <label>Password</label>
+
+              <input type="password" required name="password" />
+              <div className="btn btn--signup">Log In to your account</div>
+            </div>
+            <p> OR </p>
+            <div className="btn btn--signup google" onClick={logGoogleUser}>
+              <FaGoogle
+                style={{ fill: "white", marginRight: "1rem", position: "relative", top: "2px" }}
+              />
+              Log in with google
+            </div>
+          </form>
         </div>
-
-        <form className="form-fields-container-login">
-          <div>
-            <label>Email</label>
-            <input type="email" required name="email" />
-            <label>Password</label>
-
-            <input type="password" required name="password" />
-            <div className="btn btn--signup">Log In to your account</div>
-          </div>
-          <p> OR </p>
-          <div className="btn btn--signup google" onClick={logGoogleUser}>
-            <FaGoogle
-              style={{ fill: "white", marginRight: "1rem", position: "relative", top: "2px" }}
-            />
-            Log in with google
-          </div>
-        </form>
       </div>
-    </>
+    </main>
   );
 };
 
